2fe0d5f575a0a0e3f6c77cc09e3982609b39da6e
megbird <meg@vercel.com>
2025-01-17 22:50:35 -0800

    modern favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,21 @@
 import '@/styles/globals.css'
 import localFont from 'next/font/local'
+import type { Metadata } from 'next'
+
+// Metadata configuration
+export const metadata: Metadata = {
+  title: 'Meg Bird',
+  description: 'megbird.me',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.svg',
+    apple: '/apple-touch-icon.png',
+    other: {
+      rel: 'mask-icon',
+      url: '/favicon.svg',
+    },
+  },
+}
 
 // Using Neue Haas as the primary font
 const neueHaas = localFont({
